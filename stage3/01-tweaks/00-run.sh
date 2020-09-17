7f51d455a02288207aa4aadc01feb46efe635f5a
Romain Bazile <gromain.baz@gmail.com>
2020-09-17 16:48:17 +0200

    create test and libraries directories in user /home/

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -3,3 +3,6 @@
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
 EOF
+
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/test"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/libraries"
