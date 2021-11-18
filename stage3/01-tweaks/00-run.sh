bda9ba27f10e79a5f2b3a99430aa3ed9e158a9f7
Romain Bazile <romain@bazile.io>
2021-11-18 21:48:47 +0100

    Add data folder

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -6,3 +6,4 @@ EOF
 
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/test"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/libraries"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data"
