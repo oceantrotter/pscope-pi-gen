42b11d2add1c5a852219f5f701095b268cd75ee2
Romain Bazile <romain@bazile.io>
2021-12-03 17:48:48 +0100

    Node-red config

--- stage3/03-node-red/00-run.sh
+++ stage3/03-node-red/00-run.sh
@@ -3,8 +3,11 @@ on_chroot << EOF
   bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-install --confirm-pi
 EOF
 
-install -v  -m 644 -d /"${ROOTFS_DIR}/etc/systemd/system/nodered.service.d/"
-install -v  -m 644 files/override.conf "${ROOTFS_DIR}/etc/systemd/system/nodered.service.d/override.conf"
+install -v -m 644 -d "${ROOTFS_DIR}/etc/systemd/system/nodered.service.d/"
+install -v -m 644 files/override.conf "${ROOTFS_DIR}/etc/systemd/system/nodered.service.d/override.conf"
+install -v -o 1000 -g 1000 files/settings.js "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.node-red/settings.js"
+install -v -o 1000 -g 1000 files/config.projects.json "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.node-red/.config.projects.json"
+install -v -o 1000 -g 1000 files/config.users.json "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.node-red/.config.users.json"
 
 on_chroot << EOF
   sudo systemctl daemon-reload
