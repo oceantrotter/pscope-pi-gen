bb9c34295706bcbc4a630d5d188b701873301e03
Romain Bazile <romain@bazile.io>
2021-12-03 17:11:18 +0100

    remove circuitpython

--- stage3/02-circuitpython/00-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-on_chroot << EOF
-sudo pip3 install RPI.GPIO
-sudo pip3 install adafruit-blinka
-sudo pip3 install adafruit-circuitpython-motorkit
-EOF
-
-install -m -o 1000 -g 1000 755 files/blinkatest.py "/home/${FIRST_USER_NAME}/test/blinkatest.py"
