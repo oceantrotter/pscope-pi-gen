cdccc0839a53db843fa15c4b0e29fc4f66f85a22
Romain Bazile <romain@bazile.io>
2021-12-03 17:03:11 +0100

    Configure python and cleanup default folders

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -2,8 +2,6 @@
 
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
+	sudo update-alternatives --install $(which python) python $(readlink -f $(which python2)) 1
+	sudo update-alternatives --install $(which python) python $(readlink -f $(which python3)) 2
 EOF
-
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/test"
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/libraries"
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data"
