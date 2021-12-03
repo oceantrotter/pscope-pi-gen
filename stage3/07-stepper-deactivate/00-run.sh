1f683095763ae5daecf67ccba9140fd8b590cf24
Romain Bazile <romain@bazile.io>
2021-12-03 18:15:47 +0100

    Stepper deactivate on boot

---
+++ stage3/07-stepper-deactivate/00-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+install -v -m 644 files/gpio-init.service "${ROOTFS_DIR}/etc/systemd/system/gpio-init.service"
+install -v -m 755 files/stepper-disable "${ROOTFS_DIR}/usr/bin/stepper-disable"
+
+on_chroot << EOF
+  systemctl enable autohotspot.service
+EOF
