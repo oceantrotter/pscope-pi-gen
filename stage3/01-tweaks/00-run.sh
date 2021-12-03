81acf92eb12521160f450d00507d3cca89696369
Romain Bazile <romain@bazile.io>
2021-12-03 18:15:31 +0100

    Be mindful of user
    sudo is not needed in chroot

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -2,6 +2,6 @@
 
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
-	sudo update-alternatives --install $(which python) python $(readlink -f $(which python2)) 1
-	sudo update-alternatives --install $(which python) python $(readlink -f $(which python3)) 2
+	update-alternatives --install $(which python) python $(readlink -f $(which python2)) 1
+	update-alternatives --install $(which python) python $(readlink -f $(which python3)) 2
 EOF
