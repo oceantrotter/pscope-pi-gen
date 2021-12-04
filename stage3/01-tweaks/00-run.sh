7adc210b6bddb6a8270f601446aedc328d78f2ff
Romain Bazile <romain@bazile.io>
2021-12-04 01:00:53 +0100

    Dynamic network config

--- stage3/01-tweaks/00-run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-on_chroot << EOF
-	update-alternatives --install $(which python) python $(readlink -f $(which python2)) 1
-	update-alternatives --install $(which python) python $(readlink -f $(which python3)) 2
-EOF
