b414d4b53c1b2dc4f8b0fd60c120eb122e11d59e
Romain Bazile <romain@bazile.io>
2021-12-04 01:00:53 +0100

    Dynamic network config

---
+++ stage3/01-network-config/01-run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+install -v -m 644 -d "${ROOTFS_DIR}/etc/hostapd/"
+install -v -m 644 files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/hostapd.conf"
+install -v -m 644 -d "${ROOTFS_DIR}/etc/default/"
+install -v -m 644 files/hostapd "${ROOTFS_DIR}/etc/default/hostapd"
+install -v -m 644 files/dnsmasq.conf "${ROOTFS_DIR}/etc/dnsmasq.conf"
+install -v -m 644 files/dhcpcd.conf "${ROOTFS_DIR}/etc/dhcpcd.conf"
+install -v -m 644 files/dhcpcd.enter-hook "${ROOTFS_DIR}/etc/dhcpcd.enter-hook"
+install -v -m 644 -d "${ROOTFS_DIR}/etc/systemd/system/"
+install -v -m 644 files/autohotspot.service "${ROOTFS_DIR}/etc/systemd/system/autohotspot.service"
+install -v -m 644 files/autohotspot.timer "${ROOTFS_DIR}/etc/systemd/system/autohotspot.timer"
+install -v -m 644 -d "${ROOTFS_DIR}/usr/bin/"
+install -v -m 755 files/autohotspotN "${ROOTFS_DIR}/usr/bin/autohotspotN"
+
+
+on_chroot << EOF
+	systemctl unmask hostapd
+	systemctl disable dnsmasq
+	systemctl disable hostapd
+	systemctl reload dnsmasq
+	systemctl enable autohotspot.service
+	systemctl enable autohotspot.timer
+EOF
