0dd96d5e67c6e9ea5c08e50378bb5716ae3c6340
Romain Bazile <romain@bazile.io>
2021-12-05 17:35:25 +0100

    Update network file access mode

--- stage3/01-network-config/01-run.sh
+++ stage3/01-network-config/01-run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
-install -v -m 644 -d "${ROOTFS_DIR}/etc/hostapd/"
+install -v -m 755 -d "${ROOTFS_DIR}/etc/hostapd/"
 install -v -m 644 files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/hostapd.conf"
-install -v -m 644 -d "${ROOTFS_DIR}/etc/default/"
+install -v -m 755 -d "${ROOTFS_DIR}/etc/default/"
 install -v -m 644 files/hostapd "${ROOTFS_DIR}/etc/default/hostapd"
 install -v -m 644 files/dnsmasq.conf "${ROOTFS_DIR}/etc/dnsmasq.conf"
-install -v -m 644 files/dhcpcd.conf "${ROOTFS_DIR}/etc/dhcpcd.conf"
+install -v -m 664 files/dhcpcd.conf "${ROOTFS_DIR}/etc/dhcpcd.conf"
 install -v -m 644 files/dhcpcd.enter-hook "${ROOTFS_DIR}/etc/dhcpcd.enter-hook"
-install -v -m 644 -d "${ROOTFS_DIR}/etc/systemd/system/"
+install -v -m 755 -d "${ROOTFS_DIR}/etc/systemd/system/"
 install -v -m 644 files/autohotspot.service "${ROOTFS_DIR}/etc/systemd/system/autohotspot.service"
 install -v -m 644 files/autohotspot.timer "${ROOTFS_DIR}/etc/systemd/system/autohotspot.timer"
-install -v -m 644 -d "${ROOTFS_DIR}/usr/bin/"
+install -v -m 755 -d "${ROOTFS_DIR}/usr/bin/"
 install -v -m 755 files/autohotspotN "${ROOTFS_DIR}/usr/bin/autohotspotN"
 
 
