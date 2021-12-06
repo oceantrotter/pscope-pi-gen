31fee32395040fdd384dbe0744f275f563e6e5f5
Romain Bazile <romain@bazile.io>
2021-12-06 23:44:59 +0100

    Bullseye provides python3.9

--- stage3/06-python/00-run.sh
+++ stage3/06-python/00-run.sh
@@ -6,8 +6,8 @@ EOF
 
 install -v -m 755 -o 1000 -g 1000 files/blinkatest.py "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/test/blinkatest.py"
 install -v -m 755 -o 1000 -g 1000 files/stepper-controller.py "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/test/stepper-controller.py"
-ln -s "/home/${FIRST_USER_NAME}/PlanktoScope/scripts/planktoscope" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/lib/python3.7/site-packages/planktoscope"
-ln -s "/home/${FIRST_USER_NAME}/PlanktoScope/scripts/shush" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/lib/python3.7/site-packages/shush"
-install -v -d "${ROOTFS_DIR}/root/.local/lib/python3.7/site-packages"
-ln -s "/home/${FIRST_USER_NAME}/PlanktoScope/scripts/planktoscope" "${ROOTFS_DIR}/root/.local/lib/python3.7/site-packages/planktoscope"
-ln -s "/home/${FIRST_USER_NAME}/PlanktoScope/scripts/shush" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/lib/python3.7/site-packages/shush"
+ln -s "/home/${FIRST_USER_NAME}/PlanktoScope/scripts/planktoscope" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/lib/python3.9/site-packages/planktoscope"
+ln -s "/home/${FIRST_USER_NAME}/PlanktoScope/scripts/shush" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/lib/python3.9/site-packages/shush"
+install -v -d "${ROOTFS_DIR}/root/.local/lib/python3.9/site-packages"
+ln -s "/home/${FIRST_USER_NAME}/PlanktoScope/scripts/planktoscope" "${ROOTFS_DIR}/root/.local/lib/python3.9/site-packages/planktoscope"
+ln -s "/home/${FIRST_USER_NAME}/PlanktoScope/scripts/shush" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/lib/python3.9/site-packages/shush"
