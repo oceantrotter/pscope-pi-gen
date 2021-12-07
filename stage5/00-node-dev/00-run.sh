598e247154e6c7de366159ed8283d48eeb6c4a85
Romain Bazile <romain@bazile.io>
2021-12-07 11:10:19 +0100

    chroot is already in rootfs_dir

--- stage5/00-node-dev/00-run.sh
+++ stage5/00-node-dev/00-run.sh
@@ -3,7 +3,7 @@
 install -v -o 1000 -g 1000 files/settings.js "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.node-red/settings.js"
 
 on_chroot << EOF
-  cd "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.node-red"
+  cd "/home/${FIRST_USER_NAME}/.node-red"
   su - "${FIRST_USER_NAME}" -c "npm install nrlint"
   su - "${FIRST_USER_NAME}" -c "npm install node-red-debugger"
   npx nrlint --init > .nrlintrc.js
