322cc5794edb454c6c1853b3a0698fe77a100608
Romain Bazile <romain@bazile.io>
2021-12-21 21:21:45 +0100

    Do the clone in chroot

--- stage3/04-repo-clone/00-run.sh
+++ stage3/04-repo-clone/00-run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
 
-sudo -u "${FIRST_USER_NAME}" git clone https://github.com/PlanktonPlanet/PlanktoScope/ "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.node-red/projects/PlanktoScope"
-sudo -u "${FIRST_USER_NAME}" ln -s ./.node-red/projects/PlanktoScope "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/PlanktoScope"
+on_chroot << EOF
+  cd /home/"${FIRST_USER_NAME}"/
+  su - "${FIRST_USER_NAME}" -c "git clone https://github.com/PlanktonPlanet/PlanktoScope/ /home/${FIRST_USER_NAME}/.node-red/projects/PlanktoScope"
+  su - "${FIRST_USER_NAME}" -c "ln -s ./.node-red/projects/PlanktoScope /home/${FIRST_USER_NAME}/PlanktoScope"
+EOF
