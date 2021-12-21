5291d98cb43dfc369a64932c6dbab09fb6e66a7f
Romain Bazile <romain@bazile.io>
2021-12-21 15:33:24 +0100

    git checkout should be done as user to avoid unwritable files

--- stage4/00-switch-repo/00-run.sh
+++ stage4/00-switch-repo/00-run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -e
-cd "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/PlanktoScope"
-git checkout pscopehat
+
+on_chroot << EOF
+  cd "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/PlanktoScope"
+  su - "${FIRST_USER_NAME}" -c "git checkout pscopehat"
+EOF
