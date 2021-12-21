9988d7f56581a82130510762d238f9a24d1219e3
Romain Bazile <romain@bazile.io>
2021-12-21 15:33:00 +0100

    Fix .ssh folder permission

--- stage5/03-network/00-run.sh
+++ stage5/03-network/00-run.sh
@@ -2,5 +2,5 @@
 
 install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
 install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
-install -v -d					"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.ssh/"
-install -v -m 644 files/authorized_keys "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.ssh/authorized_keys"
+install -v -m 0700 -owner 1000 -u 1000	"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.ssh/"
+install -v -m 0600 -owner 1000 -u 1000files/authorized_keys "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.ssh/authorized_keys"
