45e95c2bfd38b3c25ff7f258bdd2f962103d5d15
Romain Bazile <romain@bazile.io>
2021-12-22 02:05:56 +0100

    Fix su default behavior to switch folder
    `su --login` change the current folder to default home of the user. This behavior is not desired in many cases. Let's remove the `-` and explicitly switch folders.

--- stage4/00-switch-repo/00-run.sh
+++ stage4/00-switch-repo/00-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-  cd "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/PlanktoScope"
-  su - "${FIRST_USER_NAME}" -c "git checkout pscopehat"
+  cd "/home/${FIRST_USER_NAME}/PlanktoScope"
+  su "${FIRST_USER_NAME}" -c "git checkout pscopehat"
 EOF
