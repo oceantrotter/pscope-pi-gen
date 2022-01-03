314584a8135e2167b5f84246895203de836b0473
Romain Bazile <romain@bazile.io>
2022-01-03 18:38:02 +0100

    Fix faulty install command

--- stage5/03-network/00-run.sh
+++ stage5/03-network/00-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
-install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
-install -v -m 0700 -owner 1000 -u 1000	"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.ssh/"
-install -v -m 0600 -owner 1000 -u 1000files/authorized_keys "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.ssh/authorized_keys"
+install -v -d                                         "${ROOTFS_DIR}/etc/wpa_supplicant"
+install -v -m 600 files/wpa_supplicant.conf           "${ROOTFS_DIR}/etc/wpa_supplicant/"
+install -v -d -m 0700 --owner 1000 --group 1000       "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.ssh/"
+install -v -m 0600 --owner 1000 --group 1000 files/authorized_keys "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.ssh/authorized_keys"
