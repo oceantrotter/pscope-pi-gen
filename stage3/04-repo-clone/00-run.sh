5715ce4c0cd96fc4b5d19e28ba43114b7049f9d8
Romain Bazile <romain@bazile.io>
2022-01-03 18:37:30 +0100

    Simplify repo clone in stage3

--- stage3/04-repo-clone/00-run.sh
+++ stage3/04-repo-clone/00-run.sh
@@ -2,6 +2,7 @@
 
 on_chroot << EOF
   cd "/home/${FIRST_USER_NAME}/"
-  su "${FIRST_USER_NAME}" -c "git clone https://github.com/PlanktonPlanet/PlanktoScope/ /home/${FIRST_USER_NAME}/.node-red/projects/PlanktoScope"
-  su "${FIRST_USER_NAME}" -c "ln -s ./.node-red/projects/PlanktoScope /home/${FIRST_USER_NAME}/PlanktoScope"
+  su "${FIRST_USER_NAME}"
+  git clone https://github.com/PlanktonPlanet/PlanktoScope/ /home/${FIRST_USER_NAME}/.node-red/projects/PlanktoScope
+  ln -s ./.node-red/projects/PlanktoScope /home/${FIRST_USER_NAME}/PlanktoScope
 EOF
