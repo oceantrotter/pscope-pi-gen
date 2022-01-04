3b8c1947bde40a5f5c69ef100f058725a9381bec
Romain Bazile <romain@bazile.io>
2022-01-04 12:02:56 +0100

    Simplify stage5 run and su call

--- stage5/00-node-dev/00-run.sh
+++ stage5/00-node-dev/00-run.sh
@@ -4,7 +4,8 @@ install -v -o 1000 -g 1000 files/settings.js "${ROOTFS_DIR}/home/${FIRST_USER_NA
 
 on_chroot << EOF
   cd "/home/${FIRST_USER_NAME}/.node-red"
-  su "${FIRST_USER_NAME}" -c "npm install nrlint"
-  su "${FIRST_USER_NAME}" -c "npm install node-red-debugger"
-  su "${FIRST_USER_NAME}" -c "npx nrlint --init > .nrlintrc.js"
+  su "${FIRST_USER_NAME}"
+  npm install nrlint
+  npm install node-red-debugger
+  npx nrlint --init > .nrlintrc.js
 EOF
