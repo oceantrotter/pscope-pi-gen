c13fc2554c62ba0caf20254e5c5c8c3d1693e388
Romain Bazile <romain@bazile.io>
2023-05-10 15:01:57 +0200

    node-red setup fix

--- stage3/03-node-red/00-run.sh
+++ stage3/03-node-red/00-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
-on_chroot << EOF
+on_chroot <<EOF
   cd "/home/${FIRST_USER_NAME}/"
-  su "${FIRST_USER_NAME}" -c "bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-install --confirm-pi"
+  su "${FIRST_USER_NAME}" -c "bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-install --confirm-pi --no-init"
 EOF
 
 install -v -m 644 -d "${ROOTFS_DIR}/etc/systemd/system/nodered.service.d/"
@@ -10,6 +10,6 @@ install -v -o 1000 -g 1000 files/settings.js "${ROOTFS_DIR}/home/${FIRST_USER_NA
 install -v -o 1000 -g 1000 files/config.projects.json "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.node-red/.config.projects.json"
 install -v -o 1000 -g 1000 files/config.users.json "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.node-red/.config.users.json"
 
-on_chroot << EOF
+on_chroot <<EOF
   systemctl enable nodered.service
 EOF
