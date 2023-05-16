2725b16ff7eab25d1d6af95395547d0de4949854
Romain Bazile <romain@bazile.io>
2023-05-16 13:22:21 +0200

    allow outside anonymous connections to mosquitto

--- stage5/03-network/00-run.sh
+++ stage5/03-network/00-run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-install -v -d                                         "${ROOTFS_DIR}/etc/wpa_supplicant"
-install -v -m 600 files/wpa_supplicant.conf           "${ROOTFS_DIR}/etc/wpa_supplicant/"
-install -v -d -m 0700 --owner 1000 --group 1000       "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.ssh/"
+install -v -d "${ROOTFS_DIR}/etc/wpa_supplicant"
+install -v -m 600 files/wpa_supplicant.conf "${ROOTFS_DIR}/etc/wpa_supplicant/"
+install -v -d -m 0700 --owner 1000 --group 1000 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.ssh/"
 install -v -m 0600 --owner 1000 --group 1000 files/authorized_keys "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.ssh/authorized_keys"
+install -v -d "${ROOTFS_DIR}/etc/mosquitto/conf.d"
+install -v -m 644 files/mosquitto-dev.conf "${ROOTFS_DIR}/etc/mosquitto/conf.d/"
